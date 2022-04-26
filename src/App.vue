013ddba8c78d05ec7f3f01d0bfc8457c7cdca48d
wuyupei <495174699@qq.com>
2022-04-26 21:59:09 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,5 @@
-<script setup>
-import { ref, shallowRef } from 'vue';
+<script setup></script>
 
-let person = ref({ name: 'wuyupei' });
+<template>a</template>
 
-let obj = shallowRef({ name: 'liuxu', p: { name: 'p' } });
-
-console.log(person, obj);
-console.log(person.value, obj.value);
-
-setTimeout(() => {
-  console.log(person);
-}, 1000);
-
-console.log('okok');
-</script>
-
-<template>
-  <div>{{ person.name }}</div>
-  <div>{{ obj.name }}</div>
-  <div>{{ obj.p.name }}</div>
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,6 @@
-<script setup>
-import { ref } from 'vue'
+<script setup></script>
 
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
+<template>b</template>
 
 <style scoped>
 a {
